63f6c01c3282bbdfa224bae63304284379b68a2c
mario ortiz <mario.ortiz@cua.uam.mx>
2023-11-21 09:19:45 -0600

    Se agregaron cambios

---
+++ holaMundo.py
@@ -0,0 +1,7 @@
+def saludar(nombre, matricula):
+    print(f"Hola Mundo, {nombre,"Ortiz Garcia Mario"}!")
+    print(f"Tu número de matrícula es: {matricula ,"2203066284" }")
+
+if __name__ == "__main__":
+
+    saludar(nombre, matricula)
